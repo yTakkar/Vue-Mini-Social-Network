0df09374a84e84a6b26d5721b9c94f6ac7c47399
Ajeya Rengarajan <arengarajan@arengarajan-ltm.internal.salesforce.com>
2018-06-03 16:27:34 -0700

    ad

---
+++ public/js/src/components/home/ad.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='ad'>
+    <img :src='imagePath'/>
+    <p>Advertisement</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      imagePath: '/images/dutch.gif'
+    }
+  }
+}
+</script>
