437349b7302996e73ca74b8359b05b68ced4436e
Yash Nevatia <yash@ucsd.edu>
2018-06-04 00:31:12 -0700

    Adds linter (#6)
    * configures linter and fixes issues
    
    * adds linter to build script
    
    * configures eslint

--- public/js/src/sum.js
+++ public/js/src/sum.js
@@ -1,4 +1,4 @@
 function sum(a, b) {
-  return a + b;
+  return a + b
 }
-module.exports = sum;
+module.exports = sum
--- public/js/src/test/App.test.js
+++ public/js/src/test/App.test.js
@@ -1,3 +1,3 @@
 test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
-});
+  expect(1+2).toBe(3)
+})
