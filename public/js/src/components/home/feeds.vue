b9f7836eadd29215a2754ff42ecf6b83ff2109ff
Mikey Kentaro <mikeykentarocho@gmail.com>
2018-06-04 22:57:31 -0700

    added keen-ui demo component for reference

--- public/js/src/components/home/feeds.vue
+++ public/js/src/components/home/feeds.vue
@@ -2,7 +2,7 @@
 
   <div class='feeds_wrapper'>
     <ui-alert @dismiss="showAlert1 = false" v-show="showAlert1">
-       Hi everybody! This is the default alert.
+       Hi! Welcome to SpeakEasy, your private network!
     </ui-alert>
     <template v-if='feeds.length > 0'>
       <template v-for='feed in feeds'>
@@ -21,6 +21,16 @@
 import moduleMixin from '../../mixins/module-mixin'
 
 export default {
+    data() {
+        return {
+            showAlert1: true,
+        };
+    },
+    methods: {
+        resetAlerts() {
+            this.showAlert1 = true;
+        }
+    },
   mixins: [
     moduleMixin
   ],
