e4434f8ccd7ce5f1a19cbe19f53a525cc188b7e3
rachelteitelbaum <rachel.teitelbaum25@gmail.com>
2018-06-08 01:09:09 -0700

    moved ads button to sidebar

--- public/js/src/components/others/header.vue
+++ public/js/src/components/others/header.vue
@@ -5,7 +5,6 @@
       <sidebar style="display: inline"/>
     </div>
     <div class='right'>
-        <router-link :to="{ name: 'publish', params: { username: session.username }}" active-class="ha_active">SpeakEasy Ads</router-link>
         <router-link :to="{ name: 'profile', params: { username: session.username } }" active-class='ha_active'><ui-icon>person</ui-icon>Profile</router-link>
       <a href='/logout'><ui-icon>meeting_room</ui-icon>Logout</a>
     </div>
