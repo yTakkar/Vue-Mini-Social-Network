21cdc4370281a89c9f3dbe5c3a6d246843665075
rachelteitelbaum <rachel.teitelbaum25@gmail.com>
2018-06-14 16:52:22 -0700

    finally got data posts in with delete/edit icons

--- public/js/src/components/data_post/data_post.vue
+++ public/js/src/components/data_post/data_post.vue
@@ -1,15 +1,23 @@
 <template>
 
-    <div class='note'>
+    <div style="cursor:default;" class='note'>
       <div class='note_header common_header'>
         <img :src='imgSrc' alt='Your profile!!' />
-        <div class='note_h_left'>
+        <div display='inline;' class='note_h_left'>
           <span class='note_username'>{{ post.username }}</span>
           <span class='note_time'>{{ post.post_created | timeAgo }}</span>
         </div>
       </div>
       <div class='note_title'>
-        <span>{{ post.title | to-uppercase }}</span>
+        <router-link :to="{ name: 'view-post', params: { post: post.post_id } }" >
+          <div class='data_post_icons'>
+            <ui-icon>delete</ui-icon>
+            <ui-icon>edit</ui-icon>
+          </div>
+        </router-link>
+        <span>
+          {{ post.title | to-uppercase }}
+        </span>
       </div>
       <div class='note_content'>
         <span>{{ post.content | slice }}</span>
