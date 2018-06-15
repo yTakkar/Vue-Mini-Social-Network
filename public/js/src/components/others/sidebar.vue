abea22004d59218b556b904d7726f96f6ddaff6f
yash <yashplorer@gmail.com>
2018-06-14 17:54:55 -0700

    fixes sidebar name centering

--- public/js/src/components/others/sidebar.vue
+++ public/js/src/components/others/sidebar.vue
@@ -6,10 +6,10 @@
       <div style="width:150px;margin:auto;" class='profile_img_div'>
         <img id="sidenav_img" :src='myImgSrc' alt='Your Profile!!' >
       </div>
+      <router-link id="sidebar_profile_name" :to='{ name: "profile", params: { username: session.username } }' class='user_main_link'>
+       {{ session.username }}
+      </router-link>
       <table>
-        <router-link id="sidebar_profile_name" :to='{ name: "profile", params: { username: session.username } }' class='user_main_link'>
-          <tr><td>{{ session.username }}</td></tr>
-        </router-link>
         <router-link to='/' :exact='true' active-class='ha_active'>
           <tr>
             <td><ui-icon>home</ui-icon></td>
@@ -35,28 +35,6 @@
     </div>
   </div>
 </template>
-<!-- <div class='left'>
-  <table>
-    <router-link to='/' :exact='true' active-class='ha_active'>
-      <tr>
-        <td><ui-icon>home</ui-icon></td>
-        <td>Home</td>
-      </tr>
-    </router-link>
-    <router-link to='/explore' active-class='ha_active'>
-      <tr>
-        <td><ui-icon>public</ui-icon></td>
-        <td>Explore</td>
-      </tr>
-    </router-link>
-    <router-link to='/deactivate' active-class='ha_active' >
-      <tr>
-        <td><ui-icon>delete_forever</ui-icon></td>
-        <td>Deactivate</td>
-      </tr>
-    </router-link>
-  </table>
-</div> -->
 <script>
   import userMixin from '../../mixins/user-mixin'
   
